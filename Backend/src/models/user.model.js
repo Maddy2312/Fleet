7434d135ded183c00f63e2c4b88b0f4573e2dc44
Madhav Sharma <Madhav2580sharma@gmail.com>
2026-06-09 20:17:30 +1000

    chore: install dependencies including qrcode, pngjs, and yargs

--- Backend/src/models/user.model.js
+++ Backend/src/models/user.model.js
@@ -2,58 +2,62 @@ import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
-    email : {
-        type : String,
-        required : true,
-        unique : true,
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: function () {
+      return !this.googleId;
     },
-    name : {
-        type : String,
-        required : true,
-    },
-    password : {
-        type : String,
-        required : function(){
-            return !this.googleId;
-        },
-    },
-    contact : {
-        type : String,
-        required : false,
-    },
-    role : {
-        type : String,
-        enum : ["buyer", "seller"],
-        default : "buyer",
-    },
-    googleId: {
-        type: String,
-    }
+  },
+  contact: {
+    type: String,
+    required: false,
+  },
+  role: {
+    type: String,
+    enum: ["buyer", "seller"],
+    default: "buyer",
+  },
+  googleId: {
+    type: String,
+  },
+  mfaEnabled: {
+    type: Boolean,
+    default: false,
+  },
+
+  mfaSecret: {
+    type: String,
+  },
 });
 
 userSchema.pre("save", async function () {
+  // only hash if password modified
+  if (!this.isModified("password")) {
+    return;
+  }
 
-    // only hash if password modified
-    if (!this.isModified("password")) {
-        return;
-    }
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
 
-    try {
-
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-
-        return;
-
-    } catch (error) {
-        throw new Error("Error hashing password");
-    }
-
+    return;
+  } catch (error) {
+    throw new Error("Error hashing password");
+  }
 });
 
 // compare password
 userSchema.methods.comparePassword = async function (password) {
-    return await bcrypt.compare(password, this.password);
+  return await bcrypt.compare(password, this.password);
 };
 
 const userModel = mongoose.model("User", userSchema);
